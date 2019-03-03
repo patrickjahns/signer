3a24b3470cd166ecc3cf6e1b2a093f81142f08f9
Patrick Jahns <github@patrickjahns.de>
2019-03-03 13:47:05 +0100

    initial functional testing

---
+++ tests/Functional/ApiTestCase.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @author Patrick Jahns <github@patrickjahns.de>
+ * @copyright Copyright (c) 2019, Patrick Jahns.
+ * @license GPL-2.0
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Signer\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiTestCase extends WebTestCase
+{
+    /**
+     * @before
+     */
+    public function setUpClient()
+    {
+        $this->client = static::createClient([], ['HTTP_ACCEPT' => 'application/json']);
+    }
+
+    /**
+     * @param Response $response
+     * @param int      $statusCode
+     */
+    protected function assertResponseCode(Response $response, $statusCode)
+    {
+        self::assertEquals($statusCode, $response->getStatusCode(), $response->getContent());
+    }
+
+    /**
+     * @param Response $response
+     * @param string   $contentType
+     */
+    protected function assertHeader(Response $response, $contentType)
+    {
+        if (method_exists(self::class, 'assertStringContainsString')) {
+            self::assertStringContainsString(
+                $contentType,
+                $response->headers->get('Content-Type'),
+                $response->headers
+            );
+        } else {
+            self::assertContains(
+                $contentType,
+                $response->headers->get('Content-Type'),
+                $response->headers
+            );
+        }
+    }
+
+    /**
+     * @param Response $response
+     */
+    protected function assertJsonHeader(Response $response)
+    {
+        self::assertHeader($response, 'application');
+        self::assertHeader($response, 'json');
+    }
+}
---
+++ tests/Functional/BasicRoutingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @author Patrick Jahns <github@patrickjahns.de>
+ * @copyright Copyright (c) 2019, Patrick Jahns.
+ * @license GPL-2.0
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Signer\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BasicRoutingTest extends ApiTestCase
+{
+    public function test_root_routes_shows_404()
+    {
+        $this->client->request('GET', '/');
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_NOT_FOUND);
+        $this->assertJsonHeader($response);
+    }
+
+    public function test_unknown_routes_show_404()
+    {
+        $this->client->request('GET', '/unknown');
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_NOT_FOUND);
+        $this->assertJsonHeader($response);
+    }
+}
